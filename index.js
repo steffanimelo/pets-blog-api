51d5eccf13868ff8af5afdb05ddb0f8befb5ebac
Steffani <sartinisteffani@gmail.com>
2022-04-04 09:56:09 +0200

    updating index.js

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const app = express();
 
 const port = process.env.PORT || 5050;
 
-
 app.get("/", (req, res) => res.send("Welcome to Pets Blog API"));
 app.put("/", (req, res) => res.send("./home/post.js"));
 
-const server = app.listen(port, () =>
+app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
