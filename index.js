ac4dcfc5bf80799aff63d385dac52efdb970dcb4
Steffani <sartinisteffani@gmail.com>
2022-04-05 16:29:10 +0200

    Adding Put(update) method to CRUD operation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 const express = require("express");
 const { homedir } = require("os");
 const { resolve } = require("path");
+const { countReset } = require("console");
 const app = express();
 
 app.use(express.json());   //adding a piece of middleware to the request process in the pipeline
@@ -24,15 +25,12 @@ app.get ("/home/post/:id", (req, res) => {                          //2nd route 
 });
 
 app.post ("/home", (req, res) => {          
-    const schema = {                               //defining a schema
-        name: Joi.string().min(3).required()       //schema validation using joi
-    };
-    const result = Joi.validate (req.body, schema);
 
-    if (result.error) {                       //finishing validation to the client about name         
-        res.status(400).send(result.error.details [0].message)
-        return;
-    };
+      const {error} = validatePost (req.body);     // validating the post using object destructuring sintax
+  if (error) {                       
+      res.status(400).send(error.details [0].message)
+      return;
+  };
     const newPost = {
         id: posts.length + 1,
         name: req.body.name
@@ -40,6 +38,30 @@ app.post ("/home", (req, res) => {
     posts.push(newPost);
     res.send(posts);
 });
+
+app.put("/home/post/:id", (req, res) => {
+    const post = posts.find(c => c.id === parseInt(req.params.id));                    //Look up the post
+    if (!post) res.status (404).send ("The post with the given ID was not found!")    //If not existing, return 404
+    
+    const schema = {                                       //validade        
+        name: Joi.string().min(3).required()       
+    };
+
+    const {error} = validatePost (req.body);     //If invalid, return 400 - Bad request / destructuring*
+    if (error) {                       
+        res.status(400).send(error.details [0].message)
+        return;
+    };
+     post.name = req.body.name;          //Update post
+    res.send(post);                          //Return the updated post
+});
+
+function validatePost (post) {
+    const schema = {                               
+        name: Joi.string().min(3).required()       
+    };
+    return Joi.validate (post, schema);
+};
 
 
 
